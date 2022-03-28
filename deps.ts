63c3743d2fcb960e749e684c0e165c8860580e65
Anthony Guan <guananthony@gmail.com>
2022-03-28 16:12:57 -0700

    Fix

--- deps.ts
+++ deps.ts
@@ -1,3 +1,4 @@
+// import { useObsidian } from 'https://deno.land/x/obsidian@v3.2.0/ObsidianWrapper/ObsidianWrapper.jsx';
 import React from 'https://dev.jspm.io/react';
 import ReactDOMServer from 'https://dev.jspm.io/react-dom/server';
 import ReactDOM from 'https://dev.jspm.io/react-dom';
@@ -8,11 +9,13 @@ import ReactDOM from 'https://dev.jspm.io/react-dom';
 
 // import BrowserCache from '../obsidian/src/Browser/CacheClassBrowser.js';
 
-import {
-  ObsidianWrapper,
-  useObsidian
-} from '../obsidian/ObsidianWrapper/ObsidianWrapper.jsx';
+import { ObsidianWrapper, useObsidian, BrowserCache } from 'https://deno.land/x/obsidian@v.4.0.0/clientMod.ts';
 
-import BrowserCache from '../obsidian/src/Browser/CacheClassBrowser.js';
+// import {
+//   ObsidianWrapper,
+//   useObsidian
+// } from '../obsidian/ObsidianWrapper/ObsidianWrapper.jsx';
+
+// import BrowserCache from '../obsidian/src/Browser/CacheClassBrowser.js';
 
 export { React, ReactDOMServer, ReactDOM ,ObsidianWrapper, useObsidian, BrowserCache};
